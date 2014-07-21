54f183bd64a7baf2013cd8d3786f4a7774941d75
Danilo Blera <danilo.cblera@gmail.com>
2014-07-20 21:16:03 -0300

    adiciona cálculo de pontuação
    cálculo adicionado à função principal

--- proj2.py
+++ proj2.py
@@ -5,14 +5,29 @@ a = open("apostas.txt", "r")
 apostas = a.readlines()
 a.close()
 
+import Pontos
+
+j = []
+for i in jogos[1:]:
+    j.append([int(i.split()[1]), int(i.split()[2])])
+
 qtdJog = len(jogos) + 1
 a = []
-somaPontos = 0
-for i in range(0, len(apostas), len(apostas)/qtdJog + 1):
-    temp = []
-    temp.append(apostas(i))
-    
-
+##for i in range(0, len(apostas), len(apostas)/qtdJog + 1):
+for x in range(0, len(apostas), qtdJog):
+    somaPontos = 0
+##    temp = []
+    print apostas[x]
+##    for j in range(i+1, i+qtdJog-1):
+    for y in range(1, qtdJog-1):
+        print y, apostas[y+x][:-1]
+        somaPontos = somaPontos + Pontos.pontos(j[y-1],[int(apostas[y+x].split()[1]), int(apostas[y+x].split()[2])])
+        print Pontos.pontos(j[y-1],[int(apostas[y+x].split()[1]), int(apostas[y+x].split()[2])])
+    temp = [apostas[x][:-1], somaPontos]
+    a.append(temp)
+##        print [int(apostas[y+x].split()[1]), int(apostas[y+x].split()[2])]
+##        somaPontos = somaPontos + Pontos.pontos(j[y],[apostas[y][1], [apostas[y][2]]])
+##        somaPontos = somaPontos + pontos([
 ##j = []
 ##for i in jogos[1:]:
 ##    temp = []
